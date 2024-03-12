a9182a2031331d6d52b9d5e77471c713aa6a552a
snowypark <partsnowy@gmail.com>
2024-03-12 16:31:05 +0900

    library project

---
+++ src/main/java/com/study/library/controller/HealthCheckController.java
@@ -0,0 +1,22 @@
+package com.study.library.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthCheckController {
+
+    @Value("${server.name}")
+    private String serverName;
+
+    @GetMapping("/server/health")
+    public ResponseEntity<?> check() {
+
+        return ResponseEntity.ok(Map.of("serverName", serverName));
+
+    }
+}
